68646e71222d233a3b28c38b3a3dc3b91cf410e1
Margaux <cortier.margaux@gmail.com>
2018-12-04 11:59:00 +0100

    add column illness

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -62,11 +62,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :description)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :description, :illness)
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :description)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :description, :illness)
   end
 
 end
