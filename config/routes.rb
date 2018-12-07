1ac5c069226fb09f013de31f01a0ceaca07262be
Hélène <helene.cbm@gmail.com>
2018-12-07 16:57:53 +0100

    fix_profile

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   get '/users/:id', to: 'profile#show'
-  get '/topic', to: 'home#topic_02'
+  
  
   get '/new_account', to: 'home#new_account_04'
 end
