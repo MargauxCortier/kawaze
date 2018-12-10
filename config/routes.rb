4b4c95e4a7a927391e8a8bcbf42cd299d0e59349
EliSparow <max.pp@hotmail.fr>
2018-12-11 00:31:40 +0100

    added nested routes for forum

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,21 @@ Rails.application.routes.draw do
 	
   # get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'categories#index'
   get '/topic/new', to: 'home#new_topic_03'
   get '/topic', to: 'home#topic_02'
   get '/new_account', to: 'home#new_account_04'
+  get '/categories/forums/:id', to:'forums#show'
+
+  root 'categories#index'
+
+  resources :categories do
+    resources :forums do
+  		resources :posts do
+  			resources :comments
+  		end
+  	end
+  end
 end
+
+
