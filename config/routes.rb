45dfbddee614f4735aa71edc1ccce0ea0c35cae5
EliSparow <max.pp@hotmail.fr>
2018-12-11 18:09:36 +0100

    rectified routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,13 @@ Rails.application.routes.draw do
 	
   # get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'categories#index'
+  root 'home#index'
   get '/topic/new', to: 'home#new_topic_03'
   get '/topic', to: 'home#topic_02'
   get '/new_account', to: 'home#new_account_04'
   get '/categories/forums/:id', to:'forums#show'
 
-  root 'categories#index'
-
+# Forum routes
   resources :categories do
     resources :forums do
   		resources :posts do
