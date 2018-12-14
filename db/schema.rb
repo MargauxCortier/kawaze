73cd626ab6c864af68f6fde2c831e0586d4d25a3
EliSparow <max.pp@hotmail.fr>
2018-12-14 04:02:03 +0100

    added admin role to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_10_164817) do
+ActiveRecord::Schema.define(version: 2018_12_13_235801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2018_12_10_164817) do
     t.text "description"
     t.string "illness"
     t.string "city"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
