3cb853f4c368d803c20615253ab59b3a68c06d1f
EliSparow <max.pp@hotmail.fr>
2018-12-15 21:15:57 +0100

    add likes and disslikes for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_14_234910) do
+ActiveRecord::Schema.define(version: 2018_12_15_193928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,11 +29,29 @@ ActiveRecord::Schema.define(version: 2018_12_14_234910) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "disslikes", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_disslikes_on_post_id"
+    t.index ["user_id"], name: "index_disslikes_on_user_id"
+  end
+
   create_table "forums", force: :cascade do |t|
     t.string "title"
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -72,5 +90,9 @@ ActiveRecord::Schema.define(version: 2018_12_14_234910) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "disslikes", "posts"
+  add_foreign_key "disslikes", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "messages", "users"
 end
