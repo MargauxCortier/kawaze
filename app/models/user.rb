5f13876d1e10109e5948cfeee07f918912409ae3
EliSparow <max.pp@hotmail.fr>
2018-12-15 02:43:45 +0100

    added pusher requirements adn online status

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,23 @@ class User < ApplicationRecord
   has_many :comments,  dependent: :destroy
   		# dependent: :destroy means the comments related
       # to the specific user in mention get deleted if the user does.
-
-
-
 	has_many :messages, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: :true, uniqueness: { case_sensitive: false }
+  validates :is_signed_in, inclusion: [true, false]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  after_create :notify_pusher
+
+  def notify_pusher
+    Pusher.trigger('activity', 'login', self.as_json)
+  end
+
+  def as_json(options={})
+    super(
+      only: [:id, :email, :username]
+    )
+  end
 end
