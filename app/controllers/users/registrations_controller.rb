794a145e027272105bbc5534e21476cc5a447ab3
EliSparow <max.pp@hotmail.fr>
2018-12-15 02:43:11 +0100

    edited to add pusher requirements and online status

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -62,11 +62,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :description, :illness, :city)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :description, :illness, :city, :is_signed_in, :admin)
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :description, :illness, :city)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :description, :illness, :city, :is_signed_in, :admin)
   end
 
 end
