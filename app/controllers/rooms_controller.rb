972d15b627646adc2efa701ebec051687a51a76b
Margaux <cortier.margaux@gmail.com>
2018-12-16 16:26:07 +0100

    working on automatic reload jquery

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,13 @@
 class RoomsController < ApplicationController
+
+	respond_to? :html, :js
+  
   def show
-  	@messages = Message.all
+  	@messages = Message.all	
+  end
+
+  def reload_bottom
   	
   end
+
 end
