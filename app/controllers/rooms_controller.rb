140f4b43d946ffd22c915d3638d6deaecda3bc95
Margaux <cortier.margaux@gmail.com>
2018-12-16 19:06:45 +0100

    tryer do add methode for jquery

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,4 +3,14 @@ class RoomsController < ApplicationController
   	@messages = Message.all
   	
   end
+
+  def reload_bottom
+    @message = Message.find(params[:id])
+    if @message.user == current_user && @message.rented == false
+      @message.toggle(:available)
+      @message.save
+    end
+    @message
+  end
+
 end
